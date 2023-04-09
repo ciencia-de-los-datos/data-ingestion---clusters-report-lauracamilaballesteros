a786fc02cf56f7b07c0d8f76999bb139a323d560
lauracamilaballesteros <92760450+lauracamilaballesteros@users.noreply.github.com>
2023-04-09 17:53:56 -0500

    Lab terminado

--- pregunta.py
+++ pregunta.py
@@ -12,10 +12,29 @@ espacio entre palabra y palabra.
 import pandas as pd
 
 
+import re
+
+ #Sustituir los espacios en blanco en uno solo
+def sustituir_espacio(texto):
+  pattern = re.compile(r'\s+')
+  texto = re.sub(pattern = pattern, repl= ' ',string = texto)
+  texto = re.sub(r'\.', repl= '',string = texto)
+  return texto
+
+
 def ingest_data():
 
     #
     # Inserte su código aquí
     #
+    
+    df = pd.read_fwf("clusters_report.txt", skiprows=4, header = None)
+    
+    df.columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    
+    df['principales_palabras_clave']=df.fillna(method='ffill').groupby('cluster')['principales_palabras_clave'].transform(lambda x: ' '.join(x))
+    df=df.dropna().reset_index(drop=True)
+    
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(sustituir_espacio)
 
     return df
